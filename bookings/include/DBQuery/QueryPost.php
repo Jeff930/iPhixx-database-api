f8b947747ee90d46ab373b2353b836a2983b353d
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-08-16 15:27:37 +0800

    Fixed login queries

--- bookings/include/DBQuery/QueryPost.php
+++ bookings/include/DBQuery/QueryPost.php
@@ -205,11 +205,11 @@ class DBQuery
 		
 		$password = md5($body["password"]);
 
-		$sql = "SELECT * FROM `customers` where `email` = '{$body["email"]}' 
-		and `password` = '{$password}'";
+		$sql = "SELECT * FROM `agent_username` where `agent_email` = '{$body["email"]}' 
+		and `agent_password` = '{$password}'";
 		
 		$result = mysqli_query($this->db,$sql);
-		$row=mysqli_fetch_all($result,MYSQLI_ASSOC);
+		$row['agent_username']=mysqli_fetch_all($result,MYSQLI_ASSOC);
 		return $row;
 	}
 
