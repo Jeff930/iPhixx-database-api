727d568efa0adb140a265e3130c2ba0e30d8b277
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-08-19 20:16:33 +0800

    Successfully retrieve customer ID

--- bookings/include/DBQuery/QueryPost.php
+++ bookings/include/DBQuery/QueryPost.php
@@ -222,8 +222,8 @@ class DBQuery
 
 	public function addCustomer($body){
 		$sql = "INSERT INTO `customers` (`customer_id`, `customer_fname`, `customer_lname` , `email`, `phone`,`altPhone`, `birthdate`) VALUES (NULL, '{$body["firstName"]}', '{$body["lastName"]}' ,  '{$body["email"]}', '{$body["mobile"]}','{$body["phone"]}', '{$body["birthdate"]}')";
-		$row["customer"] = mysqli_query($this->db, $sql);
-		if ($row["customer"]) {
+		$result = mysqli_query($this->db, $sql);
+		if ($result) {
     		$row["id"] = mysqli_insert_id($this->db);
 			} else {
    				 echo "Error: " . $sql . "<br>" . mysqli_error($this->db);
