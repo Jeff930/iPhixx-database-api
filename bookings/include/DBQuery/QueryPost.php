f36eeb5311215f82ca5412ccf0c249c161da78b0
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-15 15:52:59 +0800

    fetch bug fix

--- bookings/include/DBQuery/QueryPost.php
+++ bookings/include/DBQuery/QueryPost.php
@@ -430,7 +430,8 @@ class DBQuery
 	public function getModels($body){
 		$sql = "SELECT * FROM `phone_models` WHERE `devtype_id` = '{$body['email']}' AND `phonebrand_id` = '{$body['password']}' ";
 		$result = mysqli_query($this->db,$sql);
-		return $result;
+		$row=mysqli_fetch_all($result,MYSQLI_ASSOC);
+		return $row;
 	}
 
 
