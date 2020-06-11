a592fb37a6d072db530b3e3fcc705bf2631db08d
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-06-11 16:18:14 +0800

    Created Add Devtype Image API

--- bookings/include/DBQuery/QueryPost.php
+++ bookings/include/DBQuery/QueryPost.php
@@ -248,11 +248,23 @@ class DBQuery
 		return $result;
 	}
 
+	public function addDevtypeImage($body){
+		$dir = '../../../images/devtypes';
+		if ( !file_exists($dir) ) {
+     		mkdir ($dir, 0744);
+ 		}
+		$imagePath=$dir.$body['type'].'.jpg';
+		$data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $body['image']));
+		return file_put_contents($imagePath,$data);
+	}
+
 	public function addNetwork($body){
 		$sql = "INSERT INTO `carriers` (`carrier_no`, `carrier_name`) VALUES (NULL, '{$body["network_name"]}')";
 		$result = mysqli_query($this->db, $sql);
 		return $result;
 	}
+
+	
 
 	public function updateCustomer($id , $body){
 		$sql = "UPDATE `customers` SET `customer_fname` = '{$body["customer_fname"]}', `customer_lname` = '{$body["customer_lname"]}', `email` = '{$body["email"]}', `phone` = '{$body["phone"]}', `birthdate` = '{$body["birthdate"]}', `location_id` = '{$body["location_id"]}', `smsOption` = '{$body["smsOption"]}' WHERE `customers`.`customer_id` = {$id}";
