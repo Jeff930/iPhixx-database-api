425f02f53fd05b14cb150c807d66820435c6d25b
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-06-16 15:19:43 +0800

    Created Enable Brand API endpoint

--- bookings/v1/bookings/index.php
+++ bookings/v1/bookings/index.php
@@ -666,6 +666,14 @@ $app = new \Slim\App;
 		}
 	});
 
+	$app->put('/enable-brand/{id}',function(Request $request, Response $response, array $args) use($query){
+		$id = $args['id'];	
+		$result = $query->enableBrand($id);
+		if($result){
+			return json_encode($result);
+		}
+	});
+
 	$app->put('/enable-agent/{id}',function(Request $request, Response $response, array $args) use($query){
 		$id = $args['id'];	
 		$result = $query->enableAgent($id);
